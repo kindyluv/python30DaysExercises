bdb8e850b0c29a24be7ea02e7e9f8b8630ada7ce
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 14:07:04 +0100

    dayOneExercises

---
+++ dayOneExercises/formatting_string.py
@@ -0,0 +1,28 @@
+# Calculate the number of seconds a person can live
+
+age = int(input('Enter your age: '))
+
+secondsLived = 365 * 24 * 60 * 60
+
+years = age * secondsLived
+
+formatted_string = 'you have lived {}. seconds'.format(years)
+
+print(formatted_string)
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+language = 'Python'
+formated_string = 'I am {} {}. I teach {}'.format(first_name, last_name, language)
+print(formated_string)
+
+a = 4
+b = 3
+
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b))  # limits it to two digits after decimal
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
