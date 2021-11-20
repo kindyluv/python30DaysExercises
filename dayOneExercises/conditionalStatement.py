3fe7d6c8a1fb95361c8d440e4dba3aa2ebfff7e3
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 16:55:09 +0100

    dayOneExercises

--- dayOneExercises/conditionalStatement.py
+++ dayOneExercises/conditionalStatement.py
@@ -22,3 +22,18 @@ for key in person:
 
 for key, value in person.items():
     print(key, value)  # this way we get both keys and values printed out
+
+numbers = (0, 1, 2, 3, 4, 5)
+for number in numbers:
+    print(number)
+    if number == 3:
+        continue
+    print('Next number should be ', number + 1) if number != 5 else print("loop's end")  # for short hand conditions
+    # need both if and else statements
+print('outside the loop')
+
+
+for key in person:
+    if key == 'skills':
+        for skill in person['skills']:
+            print(skill)
