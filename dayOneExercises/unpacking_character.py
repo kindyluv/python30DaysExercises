df0806e1be71921c6638a56703565639400b7cfc
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 14:06:57 +0100

    dayOneExercises

---
+++ dayOneExercises/unpacking_character.py
@@ -0,0 +1,21 @@
+
+# unpacking characters
+language = 'Python'
+a, b, c, d, e, f = language  # unpacking sequence characters into variables
+print(a)  # P
+print(b)  # y
+print(c)  # t
+print(d)  # h
+print(e)  # o
+print(f)  # n
+
+items = ['he', 'she', 'me', 'them', 'us', 'then']
+A, B, C, D, E, F = items
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+print(F)
+print(items[::-1])
+print(items[1:4])
