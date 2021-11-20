ef6ce491ef95f1a1539247f47e42dc4d569e8c37
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 10:36:46 +0100

    dayOneExercises

--- dayOneExercises/PrintingOutDataTypes.py
+++ dayOneExercises/PrintingOutDataTypes.py
@@ -60,3 +60,13 @@ print(c)  # t
 print(d)  # h
 print(e)  # o
 print(f)  # n
+
+items = ['he', 'she', 'me', 'them', 'us', 'then']
+A, B, C, D, E, F = items
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+print(F)
+print(items[-2:])
