15b928fea5f76b7d29b2178ef8227849a378f7b4
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 14:02:37 +0100

    dayOneExercises

---
+++ dayOneExercises/dictionary.py
@@ -0,0 +1,17 @@
+# Dictionary
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+    }
+person.pop('first_name')        # Removes the firstname item
+person.popitem()                # Removes the address item
+del person['is_married']        # Removes the is_married item
+print(person)
