2b789e5bf985cbfaa283ea6ff41d404106e2205b
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 16:13:50 +0100

    dayOneExercises

--- dayOneExercises/dictionary.py
+++ dayOneExercises/dictionary.py
@@ -15,3 +15,19 @@ person.pop('first_name')        # Removes the firstname item
 person.popitem()                # Removes the address item
 del person['is_married']        # Removes the is_married item
 print(person)
+
+# changing dictionary to a list of items that
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print(dct.items()) # dict_items([('key1', 'value1'), ('key2', 'value2'), ('key3', 'value3'), ('key4', 'value4')])
+
+# clearing a dictionary
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+print(dct.clear()) # None
+
+
+# clearing a diction
+# syntax
+dct = {'key1':'value1', 'key2':'value2', 'key3':'value3', 'key4':'value4'}
+del dct
