5054e8e6bf40fa017f178f0dd78f4a758a74f45a
kindyluv <onyeukwuamara@gmail.com>
2021-11-20 09:49:58 +0100

    dayOneExercises

--- dayOneExercises/PrintingOutDataTypes.py
+++ dayOneExercises/PrintingOutDataTypes.py
@@ -51,3 +51,12 @@ print('{} % {} = {}'.format(a, b, a % b))
 print('{} // {} = {}'.format(a, b, a // b))
 print('{} ** {} = {}'.format(a, b, a ** b))
 
+# unpacking characters
+language = 'Python'
+a, b, c, d, e, f = language  # unpacking sequence characters into variables
+print(a)  # P
+print(b)  # y
+print(c)  # t
+print(d)  # h
+print(e)  # o
+print(f)  # n
