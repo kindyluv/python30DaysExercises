921fa81e32192f8a0d6171b8eae987f8c0ba6d28
kindyluv <onyeukwuamara@gmail.com>
2021-11-21 07:25:10 +0100

    dayOneExercises

---
+++ dayOneExercises/main.py
@@ -0,0 +1,5 @@
+from mymodule import generate_full_name, sum_two_numbers, calculate_age, weight_of_object
+print(generate_full_name('precious', 'lois'))
+print(sum_two_numbers(23, 50))
+print(calculate_age(2021, 2002))
+print(weight_of_object(50, 20))
