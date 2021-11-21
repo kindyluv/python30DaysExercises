25b9d2507d57abcc2fc4fabed66d54bd462f5a96
kindyluv <onyeukwuamara@gmail.com>
2021-11-21 07:25:06 +0100

    dayOneExercises

---
+++ dayOneExercises/math_module.py
@@ -0,0 +1,7 @@
+import math
+print(math.pi)           # 3.141592653589793, pi constant
+print(math.sqrt(2))      # 1.4142135623730951, square root
+print(math.pow(2, 3))    # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))   # 10, rounding to the highest
+print(math.log10(100))   # 2, logarithm with 10 as base
